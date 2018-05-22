6b8235523e65821700c2f17cedae2f161428f815
hakamir <rodolphe.latour@gmail.com>
2018-05-22 11:52:29 +0200

    MAJ control page XML + java

--- SamBotApplication/app/src/main/java/com/example/a8672756/sambotapplication/ControlPage.java
+++ SamBotApplication/app/src/main/java/com/example/a8672756/sambotapplication/ControlPage.java
@@ -4,16 +4,30 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class ControlPage extends AppCompatActivity {
+    ImageButton stop;
+    ImageButton up;
+    ImageButton down;
+    ImageButton right;
+    ImageButton left;
+    ImageButton option;
+    ImageButton bluetooth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_control_page);
-    }
-    public void swipeInfoPage(View v){
-        Intent intent = new Intent(ControlPage.this, InfoPage.class);
-        startActivity(intent);
+
+        stop = (ImageButton) findViewById(R.id.stop);
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
 }
