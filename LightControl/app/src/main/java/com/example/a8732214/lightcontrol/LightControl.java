98f1e0da628ba08f6d66c88d1283d0b5c2aa1bd9
MarieDonnuts <marie.donnet@laposte.net>
2018-05-24 17:37:52 +0200

    majControlLight

--- LightControl/app/src/main/java/com/example/a8732214/lightcontrol/LightControl.java
+++ LightControl/app/src/main/java/com/example/a8732214/lightcontrol/LightControl.java
@@ -3,16 +3,22 @@ package com.example.a8732214.lightcontrol;
 /* Liens utiles
 https://openclassrooms.com/courses/presentation-de-la-javadoc
 https://mathias-seguy.developpez.com/tutoriels/android/utiliser-capteurs/
+https://www.dev2qa.com/android-change-screen-brightness-use-seekbar-example/
 */
 
 import android.content.ContentResolver;
 import android.content.Context;
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -22,13 +28,23 @@ import android.util.Log;
 import android.view.WindowManager.LayoutParams;
 import android.widget.SeekBar.OnSeekBarChangeListener;
 
-public class LightControl extends AppCompatActivity{
+public class LightControl extends AppCompatActivity implements SensorEventListener{
+    // Variables declaration
     TextView textViewLight;
+    TextView textViewValueSensor;
     SeekBar seekBarLight;
     CheckBox checkBoxAutomaticLight;
 
+    //SensorManager
+    private SensorManager mySensorManager;
+    // sensor light
+    private Sensor light;
+    //Variable lux
+    float lux;
     //Variable to max brightness value
-    private int max_light = 100;
+    private int max_light = 255;
+    //Variable to min brightness value
+    private int min_light = 20;
     //Variable to store brightness value
     private int brightness;
     //Content resolver used as a handle to the system's settings
@@ -36,18 +52,16 @@ public class LightControl extends AppCompatActivity{
     //Window object, that will store a reference to the current window
     private Window window;
 
-    private SensorManager mySensorManager;
-    private Sensor light;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_light_control);
 
         //Instantiate seekbar, textview and checkbox objects
-        final TextView textViewLight = (TextView) findViewById(R.id.textViewLight);
-        CheckBox checkBoxAutomaticLight = (CheckBox) findViewById(R.id.checkBoxAutomaticLight);
-        SeekBar seekBarLight = (SeekBar) findViewById(R.id.seekBarLight);
+        textViewLight = (TextView) findViewById(R.id.textViewLight);
+        checkBoxAutomaticLight = (CheckBox) findViewById(R.id.checkBoxAutomaticLight);
+        seekBarLight = (SeekBar) findViewById(R.id.seekBarLight);
+        textViewValueSensor = (TextView) findViewById(R.id.textViewLight);
 
         //Get the content resolver
         cResolver = getContentResolver();
@@ -62,11 +76,11 @@ public class LightControl extends AppCompatActivity{
         seekBarLight.setKeyProgressIncrement(1);
 
         // Instentiate SensorManager
-        SensorManager mySensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mySensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 
         // Obtain sensor light
         // Retur NULL if it doesn't exist
-        Sensor light = mySensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        light = mySensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
 
         if (light == null) {
             Toast.makeText(this, "Pas de capteur de luminosité", Toast.LENGTH_LONG).show();
@@ -75,12 +89,7 @@ public class LightControl extends AppCompatActivity{
             Toast.makeText(this, "Il y a un capteur de luminosité", Toast.LENGTH_LONG).show();
             checkBoxAutomaticLight.setEnabled(true);
         }
-
-        if (checkBoxAutomaticLight.isChecked()){ // Automatic brightness
-
-        }else { // Seek bar brightness
-            seekBarBrightness();
-        }
+        onAutomaticOrManual();
     }
 
 
@@ -111,17 +120,17 @@ public class LightControl extends AppCompatActivity{
                 window.setAttributes(layoutpars);
             }
 
-            public void onStartTrackingTouch(SeekBar seekBar) {
+            public void onStartTrackingTouch(SeekBar seekBarLight) {
                 //Nothing handled here
             }
 
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+            public void onProgressChanged(SeekBar seekBarLight, int progress, boolean fromUser) {
                 //Set the minimal brightness level
-                //if seek bar is 20 or any value below
-                if (progress <= 20) {
-                    //Set the brightness to 20
-                    brightness = 20;
-                } else //brightness is greater than 20
+                //if seek bar is min_light or any value below
+                if (progress <= min_light) {
+                    //Set the brightness to min_light
+                    brightness = min_light;
+                } else //brightness is greater than min_light
                 {
                     //Set brightness variable based on the progress bar
                     brightness = progress;
@@ -130,11 +139,79 @@ public class LightControl extends AppCompatActivity{
                 float perc = (brightness / (float) max_light) * 100;
                 //Set the brightness percentage
                 textViewLight.setText((int) perc + " %");
+
+                Settings.System.putInt(cResolver,
+                        Settings.System.SCREEN_BRIGHTNESS_MODE,
+                        System.SCREEN_BRIGHTNESS_MODE_MANUAL);
             }
         });
     }
 
+    public void automaticBrightness(){
 
+        Settings.System.putInt(cResolver,
+                Settings.System.SCREEN_BRIGHTNESS_MODE,
+                Settings.System.SCREEN_BRIGHTNESS_MODE_AUTOMATIC);
 
+        try {
+            //Get the current system brightness
+            brightness = System.getInt(cResolver, System.SCREEN_BRIGHTNESS);
+        } catch (SettingNotFoundException e) {
+            //Throw an error case it couldn't be retrieved
+            Log.e("Error", "Cannot access system brightness");
+            e.printStackTrace();
+        }
+
+        double perc = 0.1957*lux + 2.173;
+        double brightNess = (perc / 100) * max_light;
+        brightness = (int) brightNess;
+
+        //Set the system brightness using the brightness variable value
+        System.putInt(cResolver, System.SCREEN_BRIGHTNESS, brightness);
+        //Get the current window attributes
+        LayoutParams layoutpars = window.getAttributes();
+        //Set the brightness of this window
+        layoutpars.screenBrightness = brightness / (float) max_light;
+        //Apply attribute changes to this window
+        window.setAttributes(layoutpars);
+
+        textViewLight.setText((int) perc + " %");
+        textViewValueSensor.setText("Value sensor : " + Float.toString(lux));
+    }
+
+    @Override
+    protected void onPause(){
+        mySensorManager.unregisterListener(this, light);
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume(){
+        mySensorManager.registerListener(this, light,SensorManager.SENSOR_DELAY_GAME);
+        super.onResume();
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_LIGHT){
+            lux = event.values[0];
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+        Toast.makeText(LightControl.this,"onAccurancyChanged()",Toast.LENGTH_SHORT).show();
+    }
+
+    public void onAutomaticOrManual() {
+        if (checkBoxAutomaticLight.isChecked()) { // Automatic brightness
+            seekBarLight.setEnabled(false);
+            automaticBrightness();
+            Toast.makeText(LightControl.this, "Automatic mode", Toast.LENGTH_SHORT).show();
+        } else { // Seek bar brightness
+            seekBarLight.setEnabled(true);
+            seekBarBrightness();
+            Toast.makeText(LightControl.this, "Manual mode", Toast.LENGTH_SHORT).show();
+        }
+    }
 }
-
